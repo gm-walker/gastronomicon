141f5545c1d77e1c0d9a4ef170e04d9163cfaf69
GEE <gm.walker2022@gmail.com>
2026-03-28 15:11:04 -0400

    update init script
    - update column constraints
    - insert data into metadata tables

--- init.sql
+++ init.sql
@@ -15,16 +15,17 @@ CREATE TABLE ingredients (
   name text NOT NULL,
   description text,
   volume_id int,
-  FOREIGN KEY (volume_id) REFERENCES volume(id),
   weight_id int,
+  FOREIGN KEY (volume_id) REFERENCES volume(id),
   FOREIGN KEY (weight_id) REFERENCES weight(id)
 );
 
 CREATE TABLE pairings (
   ingredientA int NOT NULL,
-  FOREIGN KEY (ingredientA) REFERENCES ingredients(id),
   ingredientB int NOT NULL,
-  FOREIGN KEY (ingredientB) REFERENCES ingredients(id)
+  PRIMARY KEY (ingredientA, ingredientB),
+  FOREIGN KEY (ingredientA) REFERENCES ingredients(id) ON DELETE CASCADE,
+  FOREIGN KEY (ingredientB) REFERENCES ingredients(id) ON DELETE CASCADE
 );
 
 CREATE TABLE seasons (
@@ -34,20 +35,22 @@ CREATE TABLE seasons (
 
 CREATE TABLE seasonality (
   ingredient int,
-  FOREIGN KEY (ingredient) REFERENCES ingredients(id),
   season int,
+  PRIMARY KEY (ingredient, season),
+  FOREIGN KEY (ingredient) REFERENCES ingredients(id),
   FOREIGN KEY (season) REFERENCES seasons(id)
 );
 
 CREATE TABLE roles (
   id int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-  name character(16)
+  name text
 );
 
 CREATE TABLE ingredient_roles (
   ingredient int,
-  FOREIGN KEY (ingredient) REFERENCES ingredients(id),
   role int,
+  PRIMARY KEY (ingredient, role),
+  FOREIGN KEY (ingredient) REFERENCES ingredients(id) ON DELETE CASCADE,
   FOREIGN KEY (role) REFERENCES roles(id)
 
 );
@@ -58,11 +61,48 @@ CREATE TABLE tastes (
 );
 
 CREATE TABLE ingredient_tastes (
-  ingredient int,
-  FOREIGN KEY (ingredient) REFERENCES ingredients(id),
-  taste int,
+  ingredient int NOT NULL,
+  taste int NOT NULL,
+  PRIMARY KEY (ingredient, taste),
+  FOREIGN KEY (ingredient) REFERENCES ingredients(id) ON DELETE CASCADE,
   FOREIGN KEY (taste) REFERENCES tastes (id)
 );
 
 
 -- insert data into database
+
+INSERT INTO tastes (name)
+VALUES 
+  ('salty'),
+  ('sweet'),
+  ('bitter'),
+  ('sour'),
+  ('umami');
+
+INSERT INTO roles (name)
+VALUES
+  ('warming'),
+  ('cooling');
+
+INSERT INTO seasons (name)
+VALUES
+  ('spring'),
+  ('summer'),
+  ('fall'),
+  ('winter')
+
+INSERT INTO volume (name)
+VALUES
+  ('quiet'),
+  ('quiet-moderate'),
+  ('moderate'),
+  ('moderate-loud'),
+  ('loud'),
+
+INSERT INTO weight (name)
+VALUES
+  ('light'),
+  ('light-medium'),
+  ('medium'),
+  ('medium-heavy'),
+  ('heavy'),
