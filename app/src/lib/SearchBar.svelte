704350e7b8ad3655e65981683c5ea493edb14bec
GEE <gm.walker2022@gmail.com>
2026-04-02 13:28:13 -0400

    add search bar component

---
+++ app/src/lib/SearchBar.svelte
@@ -0,0 +1,10 @@
+<script>
+import { Plus } from 'lucide-svelte';
+</script>
+
+<search>
+  <form action="" method="get">
+    <input type="search" name="ingredient-search" id="ingredient-search" placeholder="Add ingredient...">
+    <button id="ingredient-submit" type="submit"><Plus size={36}/></button>
+  </form>
+</search>
