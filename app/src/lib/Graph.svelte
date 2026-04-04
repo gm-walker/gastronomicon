a273e925bc55a41e30d44a6b4236601f6cf414ac
GEE <gm.walker2022@gmail.com>
2026-04-04 14:23:38 -0400

    add graph component

---
+++ app/src/lib/Graph.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+  // todo figure out how to reactively display d3-force graphs
+  export let data;
+</script>
+<svg></svg>
--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
   import SearchBar from '$lib/SearchBar.svelte';
+  import Graph from '$lib/Graph.svelte';
 </script>
 <SearchBar />
+<Graph {data} />
 
