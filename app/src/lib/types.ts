4e4174fa25bb124664372dda7a7077c3c0ab9ceb
GEE <gm.walker2022@gmail.com>
2026-04-16 16:09:40 -0400

    make types extend D3.js types

--- app/src/lib/types.ts
+++ app/src/lib/types.ts
@@ -1,11 +1,12 @@
-export interface Ingredient {
+import type { SimulationNodeDatum, SimulationLinkDatum } from 'd3';
+export interface Ingredient extends SimulationNodeDatum {
   name: string;
   description: string;
   volume: string;
   weight: string;
 }
 
-export interface Pairing {
-  a: string;
-  b: string;
+export interface Pairing extends SimulationLinkDatum<Ingredient> {
+  source: string;
+  target: string;
 }
