5e676f680f9d8a2868763f861fc9b50724d6fe01
GEE <gm.walker2022@gmail.com>
2026-04-19 15:40:25 -0400

    fix: remove unused store from page

--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
   import SearchBar from '$lib/SearchBar.svelte';
   import Graph from '$lib/Graph.svelte';
-  import { searchData } from '$lib/stores';
 </script>
 <SearchBar />
 <Graph />
